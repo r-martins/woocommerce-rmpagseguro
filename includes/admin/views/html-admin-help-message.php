37114f33c446c1b93179bc691b90685850411217
Ricardo Martins <ricardo@ricardomartins.net.br>
2021-09-30 11:03:26 +1000

    Melhoria: alteração em mensagens de erro com linguagem muito informal.

--- includes/admin/views/html-admin-help-message.php
+++ includes/admin/views/html-admin-help-message.php
@@ -12,6 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( apply_filters( 'woocommerce_pagseguro_help_message', true ) ) : ?>
 	<div class="updated inline woocommerce-message">
 		<p><?php echo esc_html( sprintf( __( 'Help us keep the %s plugin free making a donation or rate %s on WordPress.org. Thank you in advance!', 'woo-pagseguro-rm' ), __( 'WooCommerce PagSeguro', 'woo-pagseguro-rm' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
-		<p><a href="http://claudiosmweb.com/doacoes/" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'woo-pagseguro-rm' ); ?></a> <a href="https://wordpress.org/support/view/plugin-reviews/woocommerce-pagseguro?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'woo-pagseguro-rm' ); ?></a></p>
+		<p><a href="http://claudiosmweb.com/doacoes/" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'woo-pagseguro-rm' ); ?></a> <a href="https://wordpress.org/support/plugin/woo-pagseguro-rm/reviews/?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'woo-pagseguro-rm' ); ?></a></p>
 	</div>
 <?php endif;
