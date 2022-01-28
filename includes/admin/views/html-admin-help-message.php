3d1ee87bec8e42376fbe0809132e0f4fdfacb064
Ricardo Martins <ricardo@ricardomartins.net.br>
2022-01-28 16:46:55 +1100

    Small improvements in the admin help message with useful links

--- includes/admin/views/html-admin-help-message.php
+++ includes/admin/views/html-admin-help-message.php
@@ -11,7 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( apply_filters( 'woocommerce_pagseguro_help_message', true ) ) : ?>
 	<div class="updated inline woocommerce-message">
-		<p><?php echo esc_html( sprintf( __( 'Help us keep the %s plugin free making a donation or rate %s on WordPress.org. Thank you in advance!', 'woo-pagseguro-rm' ), __( 'WooCommerce PagSeguro', 'woo-pagseguro-rm' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
-		<p><a href="http://claudiosmweb.com/doacoes/" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'woo-pagseguro-rm' ); ?></a> <a href="https://wordpress.org/support/plugin/woo-pagseguro-rm/reviews/?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'woo-pagseguro-rm' ); ?></a></p>
+		<p><?php echo esc_html( sprintf( __( 'By using our module you are helping many NGO\'s and saving on PagSeguro taxes. Help us to make it even better and leave a %s review!', 'woo-pagseguro-rm' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
+		<p>
+            <a href="https://wordpress.org/support/plugin/woo-pagseguro-rm/reviews/?filter=5#postform" target="_blank" class="button button-primary"><?php esc_html_e( 'Leave a review', 'woo-pagseguro-rm' ); ?></a>
+            <a href="https://pagseguro.ricardomartins.net.br/compare.html" target="_blank" class="button button-secondary"><?php esc_html_e( 'How much I am saving?', 'woo-pagseguro-rm' ); ?></a>
+            <a href="https://pagsegurotransparente.zendesk.com/hc/pt-br/articles/208158763" target="_blank" class="button button-secondary"><?php esc_html_e( 'Who am I helping?', 'woo-pagseguro-rm' ); ?></a>
+            <a href="https://pagsegurotransparente.zendesk.com/hc/pt-br/" target="_blank" class="button button-secondary"><?php esc_html_e( 'I need help', 'woo-pagseguro-rm' ); ?></a>
+        </p>
 	</div>
 <?php endif;
